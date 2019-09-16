fe05a8b108a4e2bbb0fe194f78c1bf88c64f155f
Daniel Matthews <daniel.r.matthews@kcl.ac.uk>
2019-09-16 10:29:04 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 import setuptools
+from Cython.Build import cythonize
+import numpy
 
-setuptools.setup(name='SMLM_analysis',
+
+setuptools.setup(
+      name='SMLM_analysis',
       version='0.1',
       description='Collection of Python files for processing SMLM data',
       url='http://github.com/drmatthews/SMLM_analysis',
       author='Dan Matthews',
       author_email='dr.dan.matthews@gmail.com',
       license='MIT',
-      packages=setuptools.find_packages(exclude=['docs', 'test_data']),     
-      zip_safe=False)
+      packages=setuptools.find_packages(exclude=['docs', 'test_data']),
+	include_dirs = [numpy.get_include(), "."],
+      ext_modules = cythonize(
+            ["smlm_analysis\\localise\\mle.pyx", 
+             "smlm_analysis\\localise\\gauss_guess.pyx",
+             "smlm_analysis\\localise\\convolution.pyx"
+             "smlm_analysis\\render\\histogram.pyx"],
+             annotate=False
+      ),
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3"
+     ],
+     python_requires='>=3.6'
+)
